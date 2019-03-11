76cf8a65943682d50efe2a6cfd42bb2b8367fd7f
TemporalDagger <juan_caguiat@dlsu.edu.ph>
2019-03-11 16:45:14 +0800

    Registration GUI

--- src/View/RegistrationWindow.java
+++ src/View/RegistrationWindow.java
@@ -1,4 +1,23 @@
 package View;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
 public class RegistrationWindow {
+	
+	@FXML
+	ComboBox quizDropDown;
+	@FXML
+	Button registerUser;
+	
+	public void quizList() {
+		
+	}
+	
+	public void registerUser() {
+		
+	}
+	
+	
 }
