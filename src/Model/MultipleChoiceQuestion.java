a95168e22c036ac8941a3491a19238a7e1f5f66e
Johanna Lim <johanna_lim@dlsu.edu.ph>
2019-03-19 15:38:15 +0800

    update

---
+++ src/Model/MultipleChoiceQuestion.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class MultipleChoiceQuestion {
+    private String prompt;
+    private ArrayList<String> choices;
+    private int answer;
+    private long startTime;
+
+    public MultipleChoiceQuestion(String prompt, ArrayList<String> choices, int answer){
+
+    }
+}
