11f9d8f6fd4a0555d988919b08aa5088bd7c7ae9
TemporalDagger <juan_caguiat@dlsu.edu.ph>
2019-03-30 18:16:24 +0800

    +

--- src/Controller/LobbyController.java
+++ src/Controller/LobbyController.java
@@ -5,11 +5,16 @@
 package Controller;
 
 import java.awt.event.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import Driver.QuizBeeApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
@@ -35,13 +40,19 @@ public class LobbyController {
     private Text selectedQuizName; // Value injected by FXMLLoader
     
     @FXML
-    void startTheQuiz(ActionEvent event) {
-
+    void startTheQuiz(ActionEvent event) throws IOException, RuntimeException {
+    	
+    	Parent root = FXMLLoader.load(getClass().getResource("/View/GUIqB.fxml"));
+        QuizBeeApplication.getStage().setScene(new Scene(root, 1075, 607));
+        QuizBeeApplication.getStage().setTitle("Quiz");
     }
 
     @FXML
-    void goingBack(ActionEvent event) {
+    void goingBack(ActionEvent event) throws IOException, RuntimeException {
 
+    	Parent root = FXMLLoader.load(getClass().getResource("/View/GUIselectQ.fxml"));
+        QuizBeeApplication.getStage().setScene(new Scene(root, 592, 391));
+        QuizBeeApplication.getStage().setTitle("Select Quiz");
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
